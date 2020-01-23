3d74f3fdf89b5da4f951036cf261c468c6d1bacd
Dave Dean <d.dean@marketinvoice.com>
2020-01-23 10:20:44 +0000

    Add initial version

---
+++ Dotty/Digits.cs
@@ -0,0 +1,38 @@
+﻿namespace Dotty
+{
+    public static class Digits
+    {
+        public static string[] Strings1 =
+        {
+            "  .  ",
+            " ..  ",
+            "  .  ",
+            "  .  ",
+            "  .  ",
+            "  .  ",
+            ".....",
+        };
+
+        public static string[] Strings3 =
+        {
+            " ... ",
+            ".   .",
+            "    .",
+            " ... ",
+            "    .",
+            ".   .",
+            " ... ",
+        };
+
+        public static string[] Strings8 =
+        {
+            " ... ",
+            ".   .",
+            ".   .",
+            " ... ",
+            ".   .",
+            ".   .",
+            " ... ",
+        };
+    }
+}
